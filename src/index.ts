213312c43cd53b1a292daf6581ffe44dcc9a537a
gceballos23 <103612856+gceballos23@users.noreply.github.com>
2022-06-06 19:46:08 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+let TamanoArreglo: number = 0;
+TamanoArreglo = Number(
+  prompt("Indique  la cantidad de nros que desea incorporar al Vector : ")
+);
+
+let arrayNro: number[] = new Array(TamanoArreglo);
+
+function getRndInteger(min, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function cargarVector(v: number[], cantidad: number) {
+  let indice: number;
+  for (indice = 0; indice < cantidad; indice++) {
+    v[indice] = getRndInteger(1, 100);
+  }
+}
+
+function mostrarVertor(v: number[], cantidad: number) {
+  for (let indice = 0; indice < cantidad; indice++) {
+    console.log("Nro :", v[indice]);
+  }
+}
+
+function valorMenor(v: number[], cantidad: number): number {
+  let nroMenorTemporal: number = v[0];
+  for (let indice = 1; indice < cantidad; indice++) {
+    if (v[indice] < nroMenorTemporal) {
+      nroMenorTemporal = v[indice];
+    }
+  }
+  return nroMenorTemporal;
+}
+
+cargarVector(arrayNro, arrayNro.length);
+mostrarVertor(arrayNro, arrayNro.length);
+console.log("El Nro Menor es:", valorMenor(arrayNro, arrayNro.length));
